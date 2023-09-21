52319516d32e6bc47979ec0c387bb216379a5c14
juan sebastian <juan.sebas-722hotmail.com>
2023-09-20 22:15:22 -0500

    auth: guards, interfaces and strategy added

---
+++ src/auth/guards/user-role.guard.ts
@@ -0,0 +1,48 @@
+import { Reflector } from '@nestjs/core';
+import { CanActivate, ExecutionContext, Injectable, BadRequestException, ForbiddenException } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { User } from '../entities/user.entity';
+import { META_ROLES } from '../decorators/role-protected.decorator';
+
+@Injectable()
+export class UserRoleGuard implements CanActivate {
+  
+  constructor(
+    private readonly reflector: Reflector
+  ) {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    
+    const validRoles: string[] = this.reflector.get( META_ROLES , context.getHandler() )
+
+    if ( !validRoles ) return true;
+    if ( validRoles.length === 0 ) return true;
+    
+    const req = context.switchToHttp().getRequest();
+    const user = req.user as User;
+
+    if ( !user ) 
+      throw new BadRequestException('User not found');
+    
+      const roleName = user.role?.name; 
+      if (!roleName || !validRoles.includes(roleName)) {
+        throw new ForbiddenException(
+          `User ${user.fullName} needs a valid role: [${validRoles}]`
+        );
+      }
+  
+      return true;
+    }
+
+      /** 
+    for (const role of user.roles ) {
+      if ( validRoles.includes( role ) ) {
+        return true;
+      }
+    }*/
+    
+    
+  }
+
